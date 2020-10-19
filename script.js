c06218e9e67d27f0f7e19e63ac0d08df7fa0d732
Alberto Liguori <a1liguorialberto@gmail.com>
2020-10-19 17:13:00 +0200

    Fixed as requested

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
-let name = prompt("Inserisci il tuo nome")
+var name = prompt("Inserisci il tuo nome")
 
-let surname = prompt("Inserisci il tuo cognome")
+var surname = prompt("Inserisci il tuo cognome")
 
-let favColor = prompt("Inserisci il tuo colore preferito")
+var favColor = prompt("Inserisci il tuo colore preferito")
 
-let pw = name + surname + favColor + "19"
+var pw = name + surname + favColor + "19"
 
-console.log(pw)
-
-
-let output = document.createElement("h1")
-output.textContent=` The worst password ever is: ${pw} `
-document.querySelector("body").appendChild(output)
+document.getElementById("pwInsicura").innerHTML= "The worst password ever is:" + pw
